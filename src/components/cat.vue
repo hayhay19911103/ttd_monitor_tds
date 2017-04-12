ec55a993b97f8229909c7e878edd77bb41488573
zby张波英 <zhang_by@ctrip.com>
2017-04-12 19:43:37 +0800

    一些样式的修改

--- src/components/cat.vue
+++ src/components/cat.vue
@@ -35,21 +35,21 @@
           <div class="row form-inline distance">
             <div class="form-group col-md-3">
               <label for="taskName">任务名称</label>
-              <input type="text" class="form-control input-sm" id="taskName" placeholder="简单的说明一下">
+              <input type="text" class="form-control input-sm" id="taskName" placeholder="简单的说明一下" v-model="taskName">
             </div>
             <div class="form-group col-md-3">
               <label>间隔时间</label>
-              <select class=" input-sm">
-                <option selected>1分钟</option>
-                <option>5分钟</option>
-                <option>10分钟</option>
-                <option>20分钟</option>
-                <option>1小时</option>
-                <option>1天</option>
+              <select class=" input-sm" v-model="timeInterval">
+                <option value="1min" selected>1分钟</option>
+                <option value="5">5分钟</option>
+                <option value="10">10分钟</option>
+                <option value="20">20分钟</option>
+                <option value="60">1小时</option>
+                <option value="24*60">1天</option>
               </select>
             </div>
             <div class="form-group col-md-3">
-              <label >开始时间</label>
+              <label>开始时间</label>
               <el-date-picker
                 v-model="startTime"
                 type="date"
@@ -63,57 +63,38 @@
             <div class="form-group col-md-12">
               <label for="appId">APP &nbsp;&nbsp; ID</label>
               <input type="text" class="form-control input-sm" id="appId" placeholder="100000445" v-model="appId"
-                     @keyup.enter="showResult">
+                     @keyup.enter="showType">
             </div>
           </div>
 
           <div class="row form-inline distance col-md-12">
             <label style="position:absolute;top: 20px;">TAG:</label>
-            <div class="checkbox " style="margin-top: 20px;margin-left: 50px">
-              <label>
-                <input type="checkbox"> Total
-              </label>
-              <label>
-                <input type="checkbox"> Failure
-              </label>
-              <label>
-                <input type="checkbox"> Failure%
-              </label>
-              <label>
-                <input type="checkbox"> Min(ms)
-              </label>
-              <label>
-                <input type="checkbox"> Max(ms)
-              </label><br>
-              <label>
-                <input type="checkbox"> Avg(ms)
-              </label>
-              <label>
-                <input type="checkbox"> 95Line(ms)
-              </label>
-              <label>
-                <input type="checkbox"> 99.9Line(ms)
-              </label>
-              <label>
-                <input type="checkbox"> Std(ms)
-              </label>
-              <label>
-                <input type="checkbox"> QPS
-              </label>
-              <label>
-                <input type="checkbox"> Percent%
-              </label>
+            <div class=" " style="margin-top: 20px;margin-left: 50px">
+              <input type="checkbox" id="Total" value="Total" v-model="checkedTags"> <label for="Total">Total</label>
+              <input type="checkbox" id="Failure" value="Failure" v-model="checkedTags"> <label for="Failure"> Failure</label>
+              <input type="checkbox" id="Failure%" value="Failure%" v-model="checkedTags"> <label for="Failure%">Failure%</label>
+              <input type="checkbox" id="Min(ms)" value="Min(ms)" v-model="checkedTags"> <label for="Min(ms)">Min(ms) </label>
+              <input type="checkbox" id="Max(ms)" value="Max(ms)" v-model="checkedTags"><label for="Max(ms)">Max(ms)</label><br>
+              <input type="checkbox" id="Avg(ms)" value="Avg(ms)" v-model="checkedTags"><label for="Avg(ms)">Avg(ms)</label>
+              <input type="checkbox" id="95Line(ms)" value="95Line(ms)" v-model="checkedTags"><label for="95Line(ms)">95Line(ms)</label>
+              <input type="checkbox" id="99.9Line(ms)" value="99.9Line(ms)" v-model="checkedTags"><label for="99.9Line(ms)">99.9Line(ms)</label>
+              <input type="checkbox" id="Std(ms)" value="Std(ms)" v-model="checkedTags"><label for="Std(ms)">Std(ms)</label>
+              <input type="checkbox" id="QPS" value="QPS" v-model="checkedTags"><label for="QPS">QPS</label>
+              <input type="checkbox" id="Percent%" value="Percent%" v-model="checkedTags"><label for="Percent%">Percent%</label>
             </div>
           </div>
           <div class="row form-inline distance col-md-12 " v-if="visible">
             <div style="border: 1px solid grey;width:80% ;float:left;padding-bottom: 10px">
               <label style="position: absolute; margin-top: 10px">Type:</label>
-              <div v-for="type in typeList" class="checkbox " style="margin-top: 10px;margin-left: 50px">
-                <label><input type="checkbox" v-model="typeName"> {{type.id}}</label>
+              <div style="margin-top: 10px;margin-left: 50px">
+                <template v-for="type in typeList">
+                  <input type="checkbox" :id={{type.id}} :value={{type.id}} v-model="checkedTypes"><label for={{type.id}}>{{type.id}}</label>
+                </template>
               </div>
             </div>
             <div>
-              <button type="button" class="btn btn-primary btn-sm " style="margin-left: 20px;margin-top: 80px" @click="showResult">确认
+              <button type="button" class="btn btn-primary btn-sm " style="margin-left: 20px;margin-top: 80px"
+                      @click="showName">确认
               </button>
             </div>
           </div>
@@ -153,16 +134,20 @@
     name: 'cat',
     data: function () {
       return {
+        taskName: "",
+        timeInterval: "",
+        startTime: "",
         appId: "",
-        typeName:[],
+        checkedTags: [],//选中的tag
+        checkedTypes: [],//选中的type
         typeList: {},//接收返回的type
         visible: false,
-        startTime:"",
+
 
       }
     },
     methods: {
-      showResult: function () {
+      showType: function () {
         this.$http({
           url: "http://10.8.85.36:8090/Cat_Api_Test/servlet/LoadCatJson",
           methods: "get",
@@ -174,6 +159,18 @@
           // error callback
         });
       },
+      showName: function () {
+        this.$http({
+          url: "http://10.8.85.36:8090/Cat_Api_Test/servlet/LoadCatJson",
+          methods: "get",
+          data: this.appId,
+        }).then(response => {
+          this.typeList = response.body.report.machines.All.types;
+          this.visible = true
+        }, response => {
+          // error callback
+        });
+      }
     }
 
   }
